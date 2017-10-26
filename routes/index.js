8fc3eb1d8c18be99bae595fdd0992b9440ccb06b
Andrew Schorr <aschorr@tumblr.com>
2017-10-26 12:06:07 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+require('dotenv').config()
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM widgets', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
