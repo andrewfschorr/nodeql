cfa691ba39054faf9b58fc6c4be7b2b55592f884
Andrew Schorr <afs35mm@gmail.com>
2017-10-29 20:56:17 -0400

    idk

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+window.APP = window.APP || {}
+APP.main = (function(namespace, global) {
+    function _init() {
+        this.dedocument.querySelectorAll('.delete'));
+
+    }
+    return {
+        init: _init
+    }
+})(window.APP || {}, this);
+
+APP.main.init();
